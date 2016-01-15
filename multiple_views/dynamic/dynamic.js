dfedd7763acc6aff66f9970add04abd6173425e3
Rien Heuver <rienheuver@gmail.com>
2016-01-15 16:38:19 +0100

    Werkt nog niet, zie dynamic:29

--- multiple_views/dynamic/dynamic.js
+++ multiple_views/dynamic/dynamic.js
@@ -1,11 +1,32 @@
-insertRestrictions = function(obj, funcs, addition)
+insertJudgeRestrictions = function(obj, funcs)
+{
+  for (funcKey in funcs)
+  {
+    var orig = obj[funcs[funcKey]];
+    //console.log(orig.toString());
+    obj[funcs[funcKey]] = function() {
+      if (getId() == "Judge")
+      {
+        throw "No access allowed";
+      }
+      var result = orig.apply(this, arguments);
+      return result;
+    }
+  }
+}
+
+insertPublicCrowdRestrictions = function(obj, funcs)
 {
   for (funcKey in funcs)
   {
     var orig = obj[funcs[funcKey]];
     obj[funcs[funcKey]] = function() {
-      addition();
+      if (getId() == "PublicCrowd")
+      {
+        throw "No access allowed";
+      }
       var result = orig.apply(this, arguments);
+      // orig wordt steeds overschreven dus hij voert uiteindelijk alleen de laatst aangepaste functie uit. Iets met eval en function.toString() doen denk ik
       return result;
     }
   }
@@ -14,6 +35,6 @@ insertRestrictions = function(obj, funcs, addition)
 getId = function()
 {
   var stack = ErrorStackParser.parse(new Error());
-  var id = stack[3].functionName;
+  var id = stack[(stack.length)-3].functionName;
   return id.substring(0,id.indexOf("."));
 }
