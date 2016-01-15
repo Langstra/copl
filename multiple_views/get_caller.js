d50825d4769f4a6faefcd0f240277a0d53050a85
Langstra <wybren.kortstra@gmail.com>
2016-01-15 11:54:42 +0100

    Created aop folder

---
+++ multiple_views/get_caller.js
@@ -0,0 +1,26 @@
+function ObjectB(){
+}
+ObjectB.prototype = {
+  funcB: function() {
+    //console.log(Hello());
+    return ObjectB.prototype.funcB.caller;
+  }
+}
+
+function ObjectA(){
+}
+ObjectA.prototype = {
+  funcA: function() {
+    var b = new ObjectB();
+    console.log(b.funcB().prototype.name);
+    return ObjectA.prototype.funcA.caller;
+  }
+}
+
+function main()
+{
+    var a = new ObjectA();
+   console.log(a.funcA().name);
+}
+
+main();
